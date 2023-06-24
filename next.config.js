0d2a4fdb990e7d5f53544c26a49c67887cf30cb3
ruslannkot <aquaaqua6858@gmail.com>
2023-06-24 23:38:30 +0800

    fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    output: "export",
-    assetPrefix: 'https://greydigitalstudio.github.io/citydom'
+    // output: "export",
+    // assetPrefix: 'https://greydigitalstudio.github.io/citydom'
 }
--- src/app/components/results/components/item/item.js
+++ src/app/components/results/components/item/item.js
@@ -36,80 +36,84 @@ const Item = (props) => {
     return (
         <div className={styles.item}>
             <Link href={`/inner?id=${props.item.id}&page=${props.pageData.page}&sort=${props.sort}&size=${props.pageData.pageSize}`}>
-            <div className={styles.item__in}>
-                <div className={styles.item__uploaded}>
-                    Внесён {getDate(new Date(props.item.createdAt))}
-                </div>
-                <img className={styles.item__img} src={image} alt="" />
-                <div className={styles.item__star}
-                    style={{ display: "none" }}
-                >
-                    <Star />
-                </div>
-                <div
-                    style={{ display: "none" }}
-                className={styles.item__someinfo}>
-                    <div className={styles.item__someleter}>
-                        C
+                <div className={styles.item__in}>
+                    
+                    <div className={styles.item__uploaded}>
+                        Внесён {getDate(new Date(props.item.createdAt))}
                     </div>
+
                     <img className={styles.item__img} src={image} alt="" />
-                    <div className={styles.item__star}>
+
+                    <div className={styles.item__star}
+                        style={{ display: "none" }}
+                    >
                         <Star />
                     </div>
-                    <div className={styles.item__someinfo}>
+
+                    <div
+                        style={{ display: "none" }}
+                    className={styles.item__someinfo}>
                         <div className={styles.item__someleter}>
                             C
                         </div>
-                        <div className={styles.item__somenumber}>
-                            6,6
+                        <img className={styles.item__img} src={image} alt="" />
+                        <div className={styles.item__star}>
+                            <Star />
+                        </div>
+                        <div className={styles.item__someinfo}>
+                            <div className={styles.item__someleter}>
+                                C
+                            </div>
+                            <div className={styles.item__somenumber}>
+                                6,6
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.item__cost}>
-                        <div className={styles.item__angleLeft}>
-                            <Angle />
+                        <div className={styles.item__cost}>
+                            <div className={styles.item__angleLeft}>
+                                <Angle />
+                            </div>
+                            <div className={styles.item__angleRight}>
+                                <Angle />
+                            </div>
+                            от {props.item.minFlatPrice} ₽ &nbsp;
+                            <CostIcon />
                         </div>
-                        <div className={styles.item__angleRight}>
-                            <Angle />
+                        <div className={styles.item__row}>
+                            <div></div>
+                            <div className={styles.item__flatscount}>
+                                {props.item.flatsCount} {
+                                    props.item.flatsCount % 100 < 20 && props.item.flatsCount % 100 > 10 ? ' квартир' :
+                                        props.item.flatsCount % 10 === 1 ? ' квартира' :
+                                            props.item.flatsCount % 10 < 5 && props.item.flatsCount % 10 > 0 ? ' квартиры' :
+                                                ' квартир'
+                                }
+
+                            </div>
                         </div>
-                        от {props.item.minFlatPrice} ₽ &nbsp;
-                        <CostIcon />
-                    </div>
-                    <div className={styles.item__row}>
-                        <div></div>
-                        <div className={styles.item__flatscount}>
-                            {props.item.flatsCount} {
-                                props.item.flatsCount % 100 < 20 && props.item.flatsCount % 100 > 10 ? ' квартир' :
-                                    props.item.flatsCount % 10 === 1 ? ' квартира' :
-                                        props.item.flatsCount % 10 < 5 && props.item.flatsCount % 10 > 0 ? ' квартиры' :
-                                            ' квартир'
-                            }
 
+                        <div className={styles.item__address}>
+                            <Pin />
+                            <div className={styles.item__address_text}>
+                                {props.item.title}
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.item__address}>
-                        <Pin />
-                        <div className={styles.item__address_text}>
-                            {props.item.title}
+                        <div className={styles.item__row}>
+                            <div className={styles.item__icons}>
+                                {props.item.isProtectedArea && <ProtectedArea />}
+                                {props.item.hasBesideSchool && <BesideSchool />}
+                                {props.item.isClosedArea && <ClosedArea />}
+                                {(props.item.hasGroundParking || props.item.hasUndergroundParking || props.item.hasBesidePark) && <Parking />}
+                                {props.item.hasCctv && <CCTV />}
+                                {props.item.hasBesidePreSchool && <BesidePreSchool />}
+
+                            </div>
+                            <div className={styles.item__quarter}>
+                                {props.item.endConstruction}
+                            </div>
                         </div>
-                    </div>
-
-                    <div className={styles.item__row}>
-                        <div className={styles.item__icons}>
-                            {props.item.isProtectedArea && <ProtectedArea />}
-                            {props.item.hasBesideSchool && <BesideSchool />}
-                            {props.item.isClosedArea && <ClosedArea />}
-                            {(props.item.hasGroundParking || props.item.hasUndergroundParking || props.item.hasBesidePark) && <Parking />}
-                            {props.item.hasCctv && <CCTV />}
-                            {props.item.hasBesidePreSchool && <BesidePreSchool />}
-
-                        </div>
-                        <div className={styles.item__quarter}>
-                            {props.item.endConstruction}
-                        </div>
-                    </div>
-
-
+                        
                 </div>
             </Link>
         </div>
